4def47bad97ce5dfc2821155dd566867363a0fcf
Shanas <nassibshaban345@gmail.com>
2022-12-02 18:34:06 +0300

    Alot of stuffs happened

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,21 +1,72 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int leftDiceNumber = 1, rightDiceNumber = 1;
+
+  void changeDiceNumber() {
+      setState(() {
+        leftDiceNumber =  Random().nextInt(6) + 1;
+        rightDiceNumber = Random().nextInt(6) + 1;
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Dicee game',
+      title: 'Dice game',
       home: Scaffold(
-        body: Text('Dicee game'),
+        appBar: AppBar(
+          backgroundColor: Colors.red.shade500,
+          centerTitle: true,
+          title: Text('Dicee'),
+        ),
+        backgroundColor: Colors.red.shade500,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child:  TextButton(child: Image.asset(
+                      'images/dice$leftDiceNumber.png',
+                    ),
+                      onPressed: () {
+                        changeDiceNumber();
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 20.0,),
+                  Expanded(
+                    child: TextButton(child: Image.asset('images/dice$rightDiceNumber.png'),
+                      onPressed: () {
+                       changeDiceNumber();
+                      },),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+
+
 
