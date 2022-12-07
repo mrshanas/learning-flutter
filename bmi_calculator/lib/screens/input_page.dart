7414e8a0c92efce5eb56f19eae2a37342c8ac28a
Shanas <nassibshaban345@gmail.com>
2022-12-08 00:33:30 +0300

    Update input_page.dart

--- bmi_calculator/lib/screens/input_page.dart
+++ bmi_calculator/lib/screens/input_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+Color cardColor = Color(0xFF1D1E33);
+Color bottomCardColor = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
 
@@ -13,21 +16,90 @@ class _InputPageState extends State<InputPage> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        leading: Icon(
+        leading: const Icon(
           Icons.menu,
         ),
         title: const Text(
           'BMI Calculator',
         ),
       ),
-      body: const Center(
-        child: Text('The UI goes here'),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: ReusableCard(
+                  colour: cardColor,
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  colour: cardColor,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: ReusableCard(
+                  colour: cardColor,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: ReusableCard(
+                  colour: cardColor,
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  colour: cardColor,
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+              child: ReusableCard(
+            cardMargin: EdgeInsets.all(2.0),
+            colour: bottomCardColor,
+            height: 80.0,
+          ))
+        ],
       ),
-      floatingActionButton: const FloatingActionButton(
-        onPressed: null,
-        child: Icon(
-          Icons.add,
-        ),
+    );
+  }
+}
+
+class ReusableCard extends StatefulWidget {
+  ReusableCard(
+      {Key? key,
+      required this.colour,
+      this.height = 200.0,
+      this.cardMargin = const EdgeInsets.all(8.0)})
+      : super(key: key);
+
+  final Color colour;
+  double height;
+  EdgeInsets cardMargin;
+
+  @override
+  State<ReusableCard> createState() => _ReusableCardState();
+}
+
+class _ReusableCardState extends State<ReusableCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: widget.height,
+      margin: widget.cardMargin,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(4.0),
+        color: widget.colour,
       ),
     );
   }
